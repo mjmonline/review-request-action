a92d755f5865c40a178502c942ee8023065897f8
Mansour Mansour <mansour@mjmonline.se>
2023-02-15 16:57:15 +0100

    do .mjs

--- src/main.mjs
+++ src/main.mjs
@@ -1,5 +1,5 @@
-const core = require("@actions/core");
-const { context, GitHub } = require("@actions/github");
+import * as core from "@actions/core";
+import { context, GitHub } from "@actions/github";
 
 async function run() {
   try {
