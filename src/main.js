acb2ba3448bdcc8664286319a02de4efcc356a19
Mansour Mansour <mansour@mjmonline.se>
2023-02-15 16:40:37 +0100

    clean up

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import * as core from "@actions/core";
+import { context, GitHub } from "@actions/github";
+
+export async function run() {
+  try {
+    const repoToken = core.getInput("repo-token", { required: true }),
+      issue: { owner: string, repo: string, number: number } = context.issue;
+
+    const client = new GitHub(repoToken);
+
+    const reviewers = core
+      .getInput("reviewers")
+      .split(",")
+      .map((a) => a.trim());
+    const teamReviewers = core
+      .getInput("team-reviewers")
+      .split(",")
+      .map((a) => a.trim());
+
+    await client.pulls.createReviewRequest({
+      owner: issue.owner,
+      repo: issue.repo,
+      pull_number: issue.number,
+      reviewers: reviewers,
+      team_reviewers: teamReviewers,
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+    throw error;
+  }
+}
+
+run();
